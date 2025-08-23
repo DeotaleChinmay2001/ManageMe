ed2a9b30c241db4486b4b2fcd1f08919630992ed
Chinmay Deotale <deotalechinmay06@gmail.com>
2025-08-23 21:06:30 +0530

    Initial commit - React Native setup

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+        <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text style={styles.title}>Chinmay App</Text>
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+
+export default App;
